d40a1690ec3755b14bff1a3179f433e0fc663e42
David Enke <david@enke.dev>
2023-10-24 10:46:17 +0200

    feat(webdmx-popup): add simple popup element

---
+++ packages/ui/src/components/ui/popup/popup.component.ts
@@ -0,0 +1,34 @@
+import { html, LitElement, type TemplateResult, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { when } from 'lit/directives/when.js';
+
+import styles from './popup.component.scss?inline';
+
+/**
+ * @element webdmx-popup
+ *
+ * @slot - The content to display inside the popup.
+ */
+@customElement('webdmx-popup')
+export class Popup extends LitElement {
+  static override readonly styles = unsafeCSS(styles);
+
+  /**
+   * The visibility state of the popup.
+   */
+  @property({ type: Boolean, reflect: true })
+  readonly visible = false;
+
+  @property({ type: String, reflect: true })
+  position: 'top' | 'right' | 'bottom' | 'left' = 'bottom';
+
+  override render(): TemplateResult {
+    return html`${when(this.visible, () => html`<slot></slot>`)}`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'webdmx-popup': Popup;
+  }
+}
