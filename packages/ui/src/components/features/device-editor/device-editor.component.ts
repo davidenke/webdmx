6655d694253164937aa18e182baa89ce0ca5f2a9
David Enke <david@enke.dev>
2023-10-25 08:32:47 +0200

    feat(webdmx-device-editor): handle click outside
    to close the parameters popup

--- packages/ui/src/components/features/device-editor/device-editor.component.ts
+++ packages/ui/src/components/features/device-editor/device-editor.component.ts
@@ -2,6 +2,7 @@ import { html, LitElement, type TemplateResult, unsafeCSS } from 'lit';
 import { customElement, eventOptions, property } from 'lit/decorators.js';
 
 import type { DeviceData } from '../../../utils/data.utils.js';
+import { isSameOrWithin } from '../../../utils/dom.utils.js';
 import type { PopupHiddenEvent } from '../../ui/popup/popup.component.js';
 import styles from './device-editor.component.scss?inline';
 
@@ -14,6 +15,8 @@ export type DeviceEditorRemoveEvent = CustomEvent<void>;
 @customElement('webdmx-device-editor')
 export class DeviceEditor extends LitElement {
   static override readonly styles = unsafeCSS(styles);
+
+  #handleClickOutside = this.handleClickOutside.bind(this);
 
   @property({ type: Boolean, reflect: true, attribute: 'parameter-editor-visible' })
   parameterEditorVisible = false;
@@ -44,9 +47,32 @@ export class DeviceEditor extends LitElement {
     this.parameterEditorVisible = false;
   }
 
+  @eventOptions({ passive: true })
+  private handleClickOutside(event: MouseEvent) {
+    // do nothing if the popup is closed
+    if (!this.parameterEditorVisible) return;
+
+    // grab the source element of the target list
+    const [target] = event.composedPath() as HTMLElement[];
+    const isHost = isSameOrWithin([this], target);
+
+    // close the popup if the click was outside
+    if (!isHost) this.parameterEditorVisible = false;
+  }
+
   #emitRemoveEvent() {
     const event = new CustomEvent('webdmx-device-editor:remove', { bubbles: true, composed: true });
     this.dispatchEvent(event);
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('click', this.#handleClickOutside, false);
+  }
+
+  override disconnectedCallback() {
+    window.removeEventListener('click', this.#handleClickOutside, false);
+    super.disconnectedCallback();
   }
 
   override render(): TemplateResult {
---
+++ packages/ui/src/utils/dom.utils.ts
@@ -0,0 +1,21 @@
+/**
+ * Utility function to check if an element (needle) is the same or within a list
+ * of elements (haystack). This comes in handy when e.g. checking for clicks
+ * outside of an element.
+ */
+export function isSameOrWithin(haystack: ArrayLike<Element>, needle: Element): boolean {
+  return Array.from(haystack).some((child) => {
+    // check if the current child is the needle
+    if (child.isSameNode(needle)) return true;
+    // check if the current child contains the needle
+    if (child.contains(needle)) return true;
+    // collect all children and light dom children
+    const children = [...Array.from(child.children), ...Array.from(child.shadowRoot?.children ?? [])];
+    // check all children recursively
+    if (children.length) {
+      return isSameOrWithin(children, needle);
+    }
+    // no match found
+    return false;
+  });
+}
