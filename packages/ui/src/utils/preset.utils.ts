e89b57bd2e8033c24850fed817c0bae205a47d69
David Enke <david@enke.dev>
2023-10-26 01:43:06 +0200

    refactor(preset.utils): improve preset mgmt

--- packages/ui/src/utils/preset.utils.ts
+++ packages/ui/src/utils/preset.utils.ts
@@ -1,22 +1,71 @@
-import type { Preset } from '@webdmx/common';
-import { DMX } from '@webdmx/controller';
+import type { Control, Preset } from '@webdmx/common';
+import { DMX, PRESET_NAMES, type PresetName } from '@webdmx/controller';
 
-export async function loadPreset(
-  presets: Record<string, Preset | null>,
-  ...names: (string | undefined)[]
-): Promise<Record<string, Preset | null>> {
-  // load preset if missing
-  const updatedPresets = names
-    // check whether preset is already loaded and a name is given
-    .filter((name) => name !== undefined && !presets[name])
-    // load preset and add it to the list
-    .reduce(
-      async (presets, name) => ({
-        ...(await presets),
-        [name!]: (await DMX.loadPreset(name!)) ?? null,
-      }),
-      Promise.resolve(presets),
+class Presets {
+  // internal cache for loaded presets
+  #loaded = PRESET_NAMES.reduce((acc, name) => ({ ...acc, [name]: null }), {}) as Record<PresetName, Preset | null>;
+
+  /**
+   * The presets that are currently loaded.
+   * Will be initialized with `null` for each known preset name.
+   */
+  get loaded() {
+    return this.#loaded;
+  }
+
+  /**
+   * Generic getter for a preset with the given name.
+   */
+  get(name: string | PresetName | undefined): Preset | undefined {
+    return this.#loaded[name as PresetName] ?? undefined;
+  }
+
+  /**
+   * The profile names of the preset with the given name.
+   */
+  getProfileNames(name?: string | PresetName): ReadonlyArray<keyof Preset['profiles']> {
+    return Object.keys(this.#loaded[name as PresetName]?.profiles ?? {});
+  }
+
+  /**
+   * Delivers the channels of the preset with the given name and profile.
+   */
+  getChannels(name?: string | PresetName, profile?: string): Preset['profiles'][string]['channels'] {
+    return this.#loaded[name as PresetName]?.profiles?.[profile!]?.channels ?? [];
+  }
+
+  /**
+   * Returns the controls of the preset with the given name.
+   */
+  getControls(name?: string | PresetName): Preset['controls'] {
+    return this.#loaded[name as PresetName]?.controls ?? {};
+  }
+
+  /**
+   * Returns a specific control of the preset with the given name.
+   */
+  getControl<R extends Control>(name?: string | PresetName, channel?: string): R | undefined {
+    return this.#loaded[name as PresetName]?.controls?.[channel!] as R | undefined;
+  }
+
+  /**
+   * Loads the preset with the given name and stores it in the cache.
+   */
+  async load(...names: (string | PresetName | undefined)[]): Promise<Array<Preset | undefined>> {
+    return Promise.all(
+      names
+        // remove duplicates and undefined values
+        .filter((name, index) => name !== undefined && names.indexOf(name) === index)
+        // check whether preset is already loaded
+        .filter((name) => !this.#loaded[name as PresetName])
+        // load preset and add it to the list
+        .map(async (name) => {
+          const preset = await DMX.loadPreset(name as PresetName);
+          this.#loaded = { ...this.#loaded, [name as PresetName]: preset ?? null };
+          return preset;
+        }),
     );
-  // return the updated presets
-  return updatedPresets;
+  }
 }
+
+export const presets = new Presets();
