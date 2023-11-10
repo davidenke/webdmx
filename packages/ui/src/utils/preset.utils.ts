18f279ca7973d437a8e4c375beab033a6ddac18a
David Enke <david@enke.dev>
2023-11-10 15:40:01 +0100

    docs: improve argument names a bit

--- packages/ui/src/utils/preset.utils.ts
+++ packages/ui/src/utils/preset.utils.ts
@@ -16,46 +16,46 @@ export class Presets {
   /**
    * Generic getter for a preset with the given name.
    */
-  get(name: string | PresetName | undefined): Preset | undefined {
-    return this.#loaded[name as PresetName] ?? undefined;
+  get(presetName: string | PresetName | undefined): Preset | undefined {
+    return this.#loaded[presetName as PresetName] ?? undefined;
   }
 
   /**
    * The profile names of the preset with the given name.
    */
-  getProfileNames(name?: string | PresetName): ReadonlyArray<keyof Preset['profiles']> {
-    return Object.keys(this.#loaded[name as PresetName]?.profiles ?? {});
+  getProfileNames(presetName?: string | PresetName): ReadonlyArray<keyof Preset['profiles']> {
+    return Object.keys(this.#loaded[presetName as PresetName]?.profiles ?? {});
   }
 
   /**
    * Delivers the channels of the preset with the given name and profile.
    */
-  getChannels(name?: string | PresetName, profile?: string): Preset['profiles'][string]['channels'] {
-    return this.#loaded[name as PresetName]?.profiles?.[profile!]?.channels ?? [];
+  getChannels(presetName?: string | PresetName, profileName?: string): Preset['profiles'][string]['channels'] {
+    return this.#loaded[presetName as PresetName]?.profiles?.[profileName!]?.channels ?? [];
   }
 
   /**
    * Returns the controls of the preset with the given name.
    */
-  getControls(name?: string | PresetName): Preset['controls'] {
-    return this.#loaded[name as PresetName]?.controls ?? {};
+  getControls(presetName?: string | PresetName): Preset['controls'] {
+    return this.#loaded[presetName as PresetName]?.controls ?? {};
   }
 
   /**
    * Returns a specific control of the preset with the given name.
    */
-  getControl<R extends Control>(name?: string | PresetName, channel?: string): R | undefined {
-    return this.#loaded[name as PresetName]?.controls?.[channel!] as R | undefined;
+  getControl<R extends Control>(presetName?: string | PresetName, channelName?: string): R | undefined {
+    return this.#loaded[presetName as PresetName]?.controls?.[channelName!] as R | undefined;
   }
 
   /**
    * Loads the preset with the given name and stores it in the cache.
    */
-  async load(...names: (string | PresetName | undefined)[]): Promise<Array<Preset | undefined>> {
+  async load(...presetNames: (string | PresetName | undefined)[]): Promise<Array<Preset | undefined>> {
     return Promise.all(
-      names
+      presetNames
         // remove duplicates and undefined values
-        .filter((name, index) => name !== undefined && names.indexOf(name) === index)
+        .filter((name, index) => name !== undefined && presetNames.indexOf(name) === index)
         // check whether preset is already loaded
         .filter((name) => !this.#loaded[name as PresetName])
         // load preset and add it to the list
