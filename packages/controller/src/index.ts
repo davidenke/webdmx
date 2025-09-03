2cd1eb3223414d6ba6c825e5ffb5f5891745b83d
David Enke <>
2025-09-03 08:51:43 +0200

    fix(controller): add missing driver exports
    and comment the purpose of each entrypoint in barrel

--- packages/controller/src/index.ts
+++ packages/controller/src/index.ts
@@ -1,3 +1,16 @@
+// universe controller
 export * from './dmx.js';
+
+// to implement your own drivers
+export * from './drivers/base/abstract.driver.js';
+export * from './drivers/base/network.driver.js';
+export * from './drivers/base/serial.driver.js';
+
+// example driver implementations
+export * from './drivers/enttec-open-dmx-usb.driver.js';
+export * from './drivers/null.driver.js';
+export * from './drivers/ws.driver.js';
+
+// effects to manipulate signals
 export * from './fx/animation.js';
 export * from './fx/easing.utils.js';
